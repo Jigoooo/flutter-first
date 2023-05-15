23767f99daa491b5127f2b1ab06d0171a5ba1c5c
kimjigoo <jiwook98@naver.com>
2023-05-15 20:58:25 +0900

    test

---
+++ android/app/src/main/kotlin/com/jigoo/flutterfirstproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jigoo.flutterfirstproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfirstproject/screen/home_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
