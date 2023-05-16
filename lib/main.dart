18fbff2085f5a8f7df4617743d9551bc1b736d10
kimjigoo <jiwook98@naver.com>
2023-05-16 21:46:08 +0900

    이미지 자동전환

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutterfirstproject/screen/home_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+      MaterialApp(
+        home: HomeScreen(),
+      )
+  );
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,93 +1,60 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController pageController = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    
+    timer = Timer.periodic(Duration(seconds: 4), (timer) {
+      int currentPage = pageController.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if(nextPage > 4) {
+        nextPage = 0;
+      }
+
+      pageController.animateToPage(nextPage, duration: Duration(milliseconds: 400), curve: Curves.linear);
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+
+    if(timer != null) {
+      timer!.cancel();
+    }
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: Container(
-          color: Colors.black,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                  Container(
-                    color: Colors.orange,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                  Container(
-                    color: Colors.yellow,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.orange,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                  Container(
-                    color: Colors.orange,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                  Container(
-                    color: Colors.yellow,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.green,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                ],
-              ),
-            ],
-          )
-        ),
+      body: PageView(
+        controller: pageController,
+        children: [1, 2, 3, 4, 5]
+            .map((e) => Image.asset(
+                  'asset/img/image_$e.jpeg',
+                  fit: BoxFit.cover,
+                ))
+            .toList(),
       ),
     );
   }
